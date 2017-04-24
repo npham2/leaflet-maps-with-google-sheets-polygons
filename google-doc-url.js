ba4fff6077908ee1fd2c88d6235c03f6656cf253
npham2 <hongnhatpham1911@gmail.com>
2017-04-24 16:59:35 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1RPDx4BHQqz6GyK0Wuk5Soqa4bYlCkOuRwB6Z51QHPLw/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1RPDx4BHQqz6GyK0Wuk5Soqa4bYlCkOuRwB6Z51QHPLw/pubhtml';
